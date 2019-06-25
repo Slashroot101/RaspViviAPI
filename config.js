e49a54d9f7cf12236e50bc20bcd378417a3050a6
Samuel Curry <samc735@gmail.com>
2019-06-25 16:37:11 -0400

    removed config from git whoops

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  db: {
-      username: 'mongodb://vivirasp:6h2L09gEUyOdoFRK1AZKvve3HxEIVB8b3sMUQ2sC5uEGXSJFqEU8zH4kXJvqmb2Z4ZquA5CfNEvOhEcfuz8UoQ==@vivirasp.documents.azure.com:10255/?ssl=true&replicaSet=globaldb&appname=@East US',
-      password: '821LWzCsHoyw9oGbReS96Vo2QSXDHZ2511qVveNKdcFEgdGSNT645Kwk9ai12MV1hZUYUTHagn2w5y9kL9IS0w==',
-      host: 'mongodb://vivirasp:6h2L09gEUyOdoFRK1AZKvve3HxEIVB8b3sMUQ2sC5uEGXSJFqEU8zH4kXJvqmb2Z4ZquA5CfNEvOhEcfuz8UoQ==@vivirasp.documents.azure.com:10255/vivirasp?ssl=true&replicaSet=globaldb&appname=@East US',
-  },
-  saltFactor: 2,
-  secretKey: 'asdqweqweqweqweqweqwe',
-};
