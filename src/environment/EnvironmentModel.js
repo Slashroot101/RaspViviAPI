25ab495485a598905514954f5fa3a26774e34d6e
Samuel Curry <samc735@gmail.com>
2019-06-28 00:23:12 -0400

    created environment model schema

---
+++ src/environment/EnvironmentModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const environmentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  createdOn: {
+    type: Date,
+    required: true,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  modelEnvironment: {
+    x: {
+      type: Number,
+      required: true,
+    },
+    y: {
+      type: Number,
+      required: true,
+    }
+  }
+});
+
+module.exports = mongoose.model('Environment', environmentSchema);
